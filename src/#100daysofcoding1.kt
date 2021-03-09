0ab0dbab9ed35329c980b96f3703b70b7fc869f5
zillahmwimali <zillahmwimmali@gmail.com>
2021-03-09 21:30:58 +0300

    firstday

---
+++ src/#100daysofcoding1.kt
@@ -0,0 +1,28 @@
+fun main() {
+    name()
+    age()
+    phoneNumber()
+    weight()
+    boolean()
+}
+fun name(){
+    var name=("Bela")
+    println(name)
+}
+fun age(){
+    var age=22
+    ++age
+    println(age)
+}
+fun phoneNumber(){
+    var phoneNumber=712345678
+    println(phoneNumber)
+}
+fun weight(){
+    var weight=58.7654
+    println(weight)
+}
+fun boolean(){
+    val citizenship=("Kenyan")
+    println("true")
+}
